c15bb9736347ed625e89aad120fc5f8eac2e7c90
lara <lara@whatever.com>
2013-04-11 15:38:29 +0300

    Add pr3

---
+++ pr3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+x = 5
+if x == 1:
+    print 'X is 1'
+elif x < 6:
+    print 'X is less than 6'
+elif x < 10:
+    print 'X is less than 10'
+else:
+    print 'X is 10 or greater'
