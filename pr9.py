d78f7aee43211d3a443ea480adc88710d598a0d5
lara <lara@whatever.com>
2013-04-12 16:44:23 +0300

    I added function in pr9.py

---
+++ pr9.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#silly example 2...still silly, but better
+def DoTwo(x,y):
+    print '%d + %d = %d ' % (x,y,x+y)
+    print '%d * %d = %d ' % (x,y,x*y)
+    print '%d - %d = %d ' % (x,y,x-y)
+    print '\n'
+DoTwo(1,2)
+DoTwo(1,4)
+DoTwo(10,5)
